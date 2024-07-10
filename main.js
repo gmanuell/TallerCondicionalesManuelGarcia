1c0f2ac32747230d684d54ef5a6bbe2833248b4e
Manuel Garcia <manuelgarcia8295@gmail.com>
2024-07-10 08:45:05 -0500

    inicio

---
+++ main.js
@@ -0,0 +1,37 @@
+
+//primer ejercicio
+
+let numeroMayor = prompt("ingrese primer numero")
+
+let numeroVerificar = prompt("ingrese numero a verificar")
+
+if (numeroMayor > numeroVerificar) {
+    console.log("primer numero digitado es mayor");
+}
+
+//segundo ejercicio
+
+if (numeroMayor === numeroVerificar) {
+    console.log("los numeros son iguales")
+}else{
+    console.log("los numeros son diferentes")
+}
+
+//tercer ejercicio
+
+if (numeroMayor == numeroVerificar) {
+    console.log("son iguales")
+}else if(numeroMayor < numeroVerificar) {
+    console.log("segundo numero digitado es mayor")
+}else{(numeroMayor > numeroVerificar)
+    console.log("primer numero digitado es mayor");
+}
+
+
+
+
+
+
+
+
+
