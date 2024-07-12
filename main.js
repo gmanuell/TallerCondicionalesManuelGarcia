163307aa4c8230a8dd90b8746a9b5339f25dc448
Manuel Garcia <manuelgarcia8295@gmail.com>
2024-07-12 08:45:13 -0500

    faltan 3 ejercicios

--- main.js
+++ main.js
@@ -92,38 +92,124 @@
 
 
 
-let nom = prompt("Ingresa tu nombre");
-let nombreVip = "Manuel";
-let entrada = prompt("¿Tienes entrada? (si/no)");
+// let nom = prompt("Ingresa tu nombre");
+// let nombreVip = "Manuel";
+// let entrada = prompt("¿Tienes entrada? (si/no)");
 
-if (nom === nombreVip) {
-    alert("Bienvenido " + nom);
-} else if (entrada === "si") {
-    let tipoEntrada = prompt("¿Pase vip, normal o no?");
-    if (tipoEntrada === "vip") {
-        alert("Bienvenido " + nom);
-    } else if (tipoEntrada === "normal") {
-        let preguntaIngreso = prompt("¿Desea utilizarla? (si/no)");
-        if (preguntaIngreso === "si") {
-            alert("Bienvenido " + nom);
-        } else {
-            alert("Gracias por su visita " + nom);
+// if (nom === nombreVip) {
+//     alert("Bienvenido " + nom);
+// } else if (entrada === "si") {
+//     let tipoEntrada = prompt("¿Pase vip, normal o no?");
+//     if (tipoEntrada === "vip") {
+//         alert("Bienvenido " + nom);
+//     } else if (tipoEntrada === "normal") {
+//         let preguntaIngreso = prompt("¿Desea utilizarla? (si/no)");
+//         if (preguntaIngreso === "si") {
+//             alert("Bienvenido " + nom);
+//         } else {
+//             alert("Gracias por su visita " + nom);
+//         }
+//     } else {
+//         alert("Gracias por su visita " + nom);
+//     }
+// } else {let comprarEntrada = prompt("¿desea comprar su entrada?(si/no)")
+//     if (comprarEntrada === "si") {
+//         let dinero = prompt("¿Cuanto dinero tienes disponible?")
+//         if (dinero >= 1000) {
+//             alert("Bienvenido " + nom)
+//         }
+//     } else {
+//         alert("Gracias por su visita " + nom);
+//     }  
+// }
+
+//Octavo ejercicio
+// let numeroIncognita = 4 
+// let numeroIngresado = parseInt(prompt("ingrese adivinanza"))
+
+// if (numeroIngresado === numeroIncognita) {
+//    console.log("ganaste"); 
+// } else {
+//     if (numeroIngresado > numeroIncognita) {
+//          alert("tu numero es mayor, vuelve a intentarlo");
+//     } else {
+//         alert("tu numero es menor, vuelve a intentarlo")
+//     }
+//     numeroIngresado = parseInt(prompt("ingrese adivinanza"))
+
+//     if (numeroIngresado === numeroIncognita) {
+//         console.log("ganaste");
+//     } else {
+//         if (numeroIngresado > numeroIncognita) {
+//         alert("tu numero es mayor, vuelve a intentarlo");
+//    } else {
+//        alert("tu numero es menor, vuelve a intentarlo")
+//    }
+//     numeroIngresado = parseInt(prompt("ingrese adivinanza"))
+
+//     if (numeroIngresado === numeroIncognita) {
+//         console.log("ganaste");
+//     } else {
+//         console.log("lo sentimos, jugamos despues");
+//     }
+// }
+// }
+
+//noveno ejercicio 
+
+
+let edadIngresada = parseInt(prompt("ingrese su edad"))
+
+if (edadIngresada <= "12") {
+    alert("Perteneces a la categoria infante")
+} else {
+  if (edadIngresada >= "13" && edadIngresada <= "18") {
+      alert("Perteneces a la categoria adolecente")
+     }  else {
+        if (edadIngresada >= "19" && edadIngresada <= "44") {
+            alert("Perteneces a la categoria mayor joven")
+           }  else {
+            if (edadIngresada >= "45" && edadIngresada <= "99") {
+                alert("Perteneces a la categoria anciano")
+               }   else {
+                if (edadIngresada >= "100") {
+                    alert("¿10esta seguro que su edad es mayor a 100años?")
+                   } 
+                }
+            }
         }
-    } else {
-        alert("Gracias por su visita " + nom);
-    }
-} else {let comprarEntrada = prompt("¿desea comprar su entrada?(si/no)")
-    if (comprarEntrada === "si") {
-        let dinero = prompt("¿Cuanto dinero tienes disponible?")
-        if (dinero >= 1000) {
-            alert("Bienvenido " + nom)
-        }
-    } else {
-        alert("Gracias por su visita " + nom);
-    }  
-}
+} 
 
 
 
+// decimo tercer ejercicio 
 
+// let nombreid = prompt("Ingresa su nombre completo")
+// let numeroid = prompt("Ingresa tu numero de identificacion")
+// let fechaNacimiento = prompt("Ingresa tu fecha de nacimiento dd/mm/aa")
+// let lugarNacimiento = prompt("Ingresa su ciudad de nacimiento")
+// let altura = prompt("Ingresa tu altura")
 
+// let mensaje = nombreid + "\n" + 
+// "numero de identidad" + " " + numeroid +"\n" + 
+// "fecha de nacimiento" + fechaNacimiento + "\n" + 
+// "lugar de nacimiento" + lugarNacimiento + "\n" + 
+// "altura" + altura + "\n" + 
+// "¿sus datos son correctos?"
+// let confirmacion = confirm(mensaje)
+// if (confirmacion) {
+//     let cedula = {
+//         nombreid: nombreid,
+//         numeroid: numeroid,
+//         fechaNacimiento: fechaNacimiento,
+//         fechaNacimiento: fechaNacimiento,
+//         lugarNacimiento: lugarNacimiento
+//     };
+
+//     console.table(cedula);
+
+//     console.log("Registro exitoso.");
+// } else {
+
+//     console.log("vuelva a intentarlo en 1 mes.");
+// }
